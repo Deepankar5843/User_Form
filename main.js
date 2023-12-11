7b6e5dcd12d6ea734122af79276724a47d90692c
Deepankar <deepankarpandey5843@gmail.com>
2023-12-11 23:57:23 +0530

    Updated feature of local Storage where we by adding new user its not rewrite

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+// var currentTimeInSeconds = Math.floor(Date.now() / 1000);
+// console.log(currentTimeInSeconds);
+
+// function addUser() {
+//     var nameInput = document.getElementById('userName');
+//     var emailInput = document.getElementById('userEmail');
+//     var userList = document.getElementById('users');
+
+//     var name = nameInput.value.trim();
+//     var email = emailInput.value.trim();
+
+//     if (name && email) {
+//         // Create a new list item for the user
+//         var userItem = document.createElement('li');
+//         userItem.textContent = 'Name: ' + name + ', Email: ' + email;
+
+//         // Append the new user item to the user list
+//         userList.appendChild(userItem);
+
+//         // Store data in sessionStorage and localStorage
+
+
+//         localStorage.setItem('Name' + currentTimeInSeconds, name);
+//         localStorage.setItem('Email' + currentTimeInSecond, email);
+
+//         // Clear the input fields
+//         nameInput.value = '';
+//         emailInput.value = '';
+//     } else {
+//         alert('Please enter both name and email.');
+//     }
+// }
+
+
+
+
 function addUser() {
     var nameInput = document.getElementById('userName');
     var emailInput = document.getElementById('userEmail');
@@ -14,9 +50,16 @@ function addUser() {
         // Append the new user item to the user list
         userList.appendChild(userItem);
 
-        // Store data in sessionStorage and localStorage
-        sessionStorage.setItem('Name', name);
-        localStorage.setItem('Email', email);
+        let ans =
+        {
+            name: nameInput.value,
+            Email: emailInput.value
+        };
+
+        let make = JSON.stringify(ans)
+
+        // Use a consistent key for storage, like the email
+        localStorage.setItem(email, make);
 
         // Clear the input fields
         nameInput.value = '';
@@ -25,6 +68,11 @@ function addUser() {
         alert('Please enter both name and email.');
     }
 }
+
+
+
+
+
 
 // Added features for storing the json fromat of object
 
@@ -42,3 +90,10 @@ console.log(localStorage.getItem('myobj'));
 let obj1 = JSON.stringify(obj);
 localStorage.setItem('mys', obj1);
 console.log(localStorage.getItem('mys'));
+
+
+// here we parsed the json format
+
+let objClear = JSON.parse(localStorage.getItem('mys'));
+console.log(localStorage.getItem('mys'));
+console.log(objClear);
